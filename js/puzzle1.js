d08874600f26b31f077f884ee8a752e7b14d4915
darwin <darwin@darwin-pc>
2016-09-21 22:38:28 -0500

    Merge origin/master

--- js/puzzle1.js
+++ js/puzzle1.js
@@ -8,15 +8,13 @@ var picture;
 var pieces = [];
 var canvasWidth;
 var canvasHeight;
-var pieceWidth;
-var pieceHeight;
 var currentPiece;
 var currentDropPiece;
 var temX;
 var temY;
 var mouse;
-var center = [];
-var screen;
+//contains the position for each piece star position and end position 
+var piecePosition = [];
 var factor;
 var paths = [];
 
@@ -30,6 +28,7 @@ function init() {
     loadPieces(paths, function () {
         console.log("size of pieces " + pieces.length);
         print();
+
     });
 }
 
@@ -53,20 +52,33 @@ function loadPieces(paths, whenLoaded) {
             imgT.src = path;
         }, 1);
     });
+    console.log("pieces length out foreach " + pieces.length);
+    //return pieces;
 
 }
 
 /*print pieces into the canvas*/
 function print() {
     //compute the ceter of each image
+    var prx, pry, wi, he;
     for (i = 0; i < pieces.length; i++) {
-        center[i] = {cx: (pieces[i].width / 2), cy: (pieces[i].height / 2)};
-        console.log("imge added {" + pieces[i].width+" : "+pieces[i].height+"} image at "+pieces[i].src );
-        console.log("center pos {"+center[i].cx+" : "+ center[i].cy+"}");
-        stage.drawImage(pieces[i], Math.random()*pieces[i].width, Math.random()*pieces[i].height, pieces[i].width*factor, pieces[i].height*factor);
+        prx = Math.floor(Math.random() * pieces[i].width);
+        pry = Math.floor(Math.random() * pieces[i].height);
+        wi = Math.floor(pieces[i].width * factor);
+        he = Math.floor(pieces[i].height * factor);
+
+        piecePosition[i] = {opx: prx, opy: pry, cx: (prx + (wi / 2)), cy: (pry + (he / 2)), wi: wi, he: he};
+        console.log("imge added {" + wi + " : " + he + "} image in " + pieces[i].src + "\n" +
+                "start position  {" + piecePosition[i].opx + " : " + piecePosition[i].opy + "}\n" +
+                "final pos  {" + (prx + wi) + " : " + (pry + he) + "}" + "\n" +
+                "center pos {" + piecePosition[i].cx + " : " + piecePosition[i].cy + "}\n");
+        printCoor(i, prx, pry, piecePosition[i].cx, piecePosition[i].cy, wi, he);
+        stage.drawImage(pieces[i], prx, pry, wi, he);
     }
-    console.log(center.length);
+    //console.log(piecePosition.length);
+    document.onmouseup = mousePosition;
 }
+
 
 /*load the main imagen and change the width and height */
 function loadImage() {
@@ -79,6 +91,29 @@ function loadImage() {
     console.log("image's dimention change {" + picture.width + " : " + picture.height + "}");
     getCanvan();
 }
+
+/*just by development status*/
+function printCoor(num, rx, ry, cx, cy, wi, he) {
+    stage.strokeStyle = "#0000FF";
+    stage.fillStyle = "#FF0000";
+    stage.strokeRect(rx, ry, wi, he);
+    stage.fillText((rx + wi) + " " + (ry + he), (rx + wi), (ry + he));
+    stage.fillText(cx + " " + cy, cx, cy);
+    stage.fillText(rx + " " + ry, rx, ry);
+//    console.log(cx);
+    stage.strokeStyle = "#00FF00";
+    stage.beginPath();
+    stage.lineWidth = 1;
+    stage.lineCap = "round";
+    stage.moveTo(cx, 0);
+    stage.lineTo(cx, 600);
+    stage.moveTo(0, cy);
+    stage.lineTo(625, cy);
+    stage.stroke();
+}
+
+/*end functions of development status*/
+
 
 function reziseImg(temImg) {
     if (temImg.height >= canvasHeight) {
@@ -100,30 +135,46 @@ function getCanvan(dif) {
     mouse = {x: 0, y: 0};
     //add mousemove event into the document
     //document.onmousemove =overCanvas;
-    document.onmouseup = mousePosition;
-    document.onmousedown = mousePosition;
+
+    //document.onmousemove = mousePosition;
 }
 
 //get the mouse position base on canvas
 function mousePosition(e) {
 
-    if (e.layerX || e.layerX === 0) {
-        mouse.x = e.layerX - canvas.offsetLeft;
-        mouse.y = e.layerY - canvas.offsetTop;
-        console.log("with e.layer by x: " + mouse.x + " by y: " + e.layerY);
-    } else if (e.offsetX || e.offsetX === 0) {
-        mouse.x = e.offsetX - canvas.offsetLeft;
-        mouse.y = e.offsetY - canvas.offsetTop;
-        console.log("with e.offset by x: " + mouse.x + " by y: " + e.layerY);
-    }
+    mouse.x = e.offsetX;
+    mouse.y = e.offsetY;
+    console.log("e.layer by x: " + e.layerX + " : " + canvas.offsetLeft + " : " + e.offsetX);
+    console.log("e.layer by x: " + e.layerY + " : " + canvas.offsetTop + " : " + e.offsetY);
+    checkPieceClicked();
     //stage.drawImage(img2, mouse.x, mouse.y, 80, 236);
     return mouse;
 }
 
-/*compute the images centers*/
+function checkPieceClicked() {
+    var i;
+    var piece;
+    console.log("pieces length " + pieces.lengt)
+    for (i = 0; i < pieces.length; i++) {
+        piece = pieces[i];
+        console.log("mouse   x: " + mouse.x + " y: " + mouse.y + "\n" +
+                "init p  x: " + piecePosition.opx + " y: " + piecePosition.opy + "\n" +
+                "final p x: " + (piecePosition.opx + piecePosition.wi) + " y: " + (piecePosition.opy + piecePosition.he));
+        if (mouse.x < piecePosition.opx && mouse.x > (piecePosition.opx + piecePosition.wi) &&
+                mouse.y < piecePosition.opy && mouse.y > (piecePosition.opy + piecePosition.he)) {
+            console.log("pieces clicked !! " + piece.src);
+            return piece;
+        } else {
+            console.log("no pieces was click !! ");
+            return null;
+        }
+    }
 
-function centerAdd(imgT, pos) {
-    center[pos] = {cx: (imgT.width / 2), cy: (imgT.height / 2)};
-    console.log("imge added " + imgT.width);
-    pieces[pos] = imgT;
+}
+
+
+/*find wich piece was clicked*/
+
+function clickPiece(e) {
+
 }
